ed61027811a093abd0e4134160330e9ab8d914c0
unknown <elekberzadeh333@gmail.com>
2020-10-05 03:54:26 +0400

    Main Section is almost ready,blackout div has height problem

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import './header.css'
 import React from 'react'
+import { images } from '../../img/images'
+import NavMenu from '../NavMenu/NavMenu'
 
 export default function Header() {
     return (
         <header className='header'>
-            
+            <img
+                className='header__logo'
+                src={images.logo}
+                alt='logo img'>
+            </img>
+            <NavMenu />
+            <div className='header__col-2'>
+                <i className="far fa-user header__user-icon" />
+                <i className="fas fa-shopping-cart header-cart-icon" />
+                <div className='header__nav-btn-container'>
+                    <div className='header__nav-btn' />
+                </div>
+                
+            </div>
         </header>
     )
 }
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,26 @@
 import './product.css'
 import React from 'react'
 
-export default function Product({name,price,imageUrl}) {
+export default function Product({ name, price, imageUrl }) {
     return (
         <div className='product'>
-            <img className='product__img'
-                src={require('../../img/clothes/hoodie-black.png')}
-                alt='product img'
-            />
             <span className='product__price-tag'>
-                {price}  RUB
+                {price} RUB
             </span>
+            <div className='product__header'>
+                <div className='product__blackout'>
+
+                </div>
+                <img className='product__img'
+                    src={require('../../img/clothes/hoodie-black.png')}
+                    alt='product img'
+                />
+                <span className='product__details-btn'>
+                    details
+            </span>
+            </div>
+
+
 
             <div className='product__name'>
                 {name}
